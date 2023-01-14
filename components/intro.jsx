8014ebc3102fcf5c625574f5ab107f37408df47e
reprereer <kokudoo0912@gmail.com>
2023-01-15 02:49:16 +0900

    サイト上部のコンポーネントにつきstyled-componentを当てた

--- components/intro.jsx
+++ components/intro.jsx
@@ -1,12 +1,84 @@
+import styled from 'styled-components'
+
 //タイトル部分
 const Intro = () => {
+  
     return (
-      <section>
-        <h1>
+        <StyledTitleBox>
+        <StyledTitle>
           Fictionaire.
-        </h1>
-      </section>
+        </StyledTitle>
+        <StyledMessage>
+        The characters and events depicted in this site are fictitious. Any similarity to actual persons, living or dead, is purely coincidental.
+        </StyledMessage>    
+        </StyledTitleBox>
     )
   }
-  
-  export default Intro
+
+export default Intro
+
+const StyledTitle = styled.h1`
+    ${({ theme }) => theme.breakpoint.media.base`
+        color: ${theme.colors.black};
+        text-align: center;
+        font-size: ${theme.fonts.size.xl};
+    `}
+    ${({ theme }) => theme.breakpoint.media.ss`
+        color: ${theme.colors.black};
+        text-align: center;
+        font-size: ${theme.fonts.size.xl3}
+    `}
+    ${({theme}) => theme.breakpoint.media.sm`
+        color: ${theme.colors.black};
+        text-align: center;
+        font-size: ${theme.fonts.size.xl4};
+    `}
+    ${({theme}) => theme.breakpoint.media.md`
+        color: ${theme.colors.black};
+        text-align: center;
+        font-size: ${theme.fonts.size.xl5};
+    `}
+    ${({theme}) => theme.breakpoint.media.lg`
+        color: ${theme.colors.black};
+        text-align: center;
+        font-size: ${theme.fonts.size.xl6};
+    `}
+    ${({theme}) => theme.breakpoint.media.xl`
+        color: ${theme.colors.black};
+        text-align: center;
+        font-size: ${theme.fonts.size.xl7};
+    `}
+`
+
+const StyledTitleBox = styled.div`
+  ${({ theme }) => theme.breakpoint.media.base`
+    display: flex;
+    gap: 10%;
+    padding: 30px;
+    align-items: center;
+    grid-area: content;
+    justify-content: center;
+    flex-direction: column;
+  `}
+  ${({ theme }) => theme.breakpoint.media.md`
+    display: flex;
+    gap: 5%;
+    padding-top: 1%;
+    padding-bottom: 1%;
+    padding-left: 10%;
+    padding-right: 10%;
+    align-items: center;
+    grid-area: content;
+    justify-content: center;
+    flex-direction: row;
+  `}
+`
+
+const StyledMessage = styled.h4`
+  ${({ theme }) => theme.breakpoint.media.base`
+    color: ${theme.colors.black};
+    text-align: left;
+    font-size: ${theme.fonts.size.base};
+    overflow-wrap: break-word;
+  `}
+`
--- components/message.jsx
+++ components/message.jsx
@@ -1,18 +1,59 @@
+import styled from 'styled-components'
+
 //サイトメッセージのための場所
 const Message = () => {
     return (
         <section>
-            <h2>
-            Theme
-            </h2>
-            <h3>
+            <StyledMessage>
             ここに載っている創作物は私の制作したフィクションです。ブログ記事も、小説も、音楽も、動画も、そしてこのサイトすらフィクションです。あなたが見ている物語のキャラクターは私とは全く関係がありません。
-            </h3>
-            <h4>
-            The characters and events depicted in this site are fictitious. Any similarity to actual persons, living or dead, is purely coincidental.
-            </h4>
+            </StyledMessage>
         </section>
     )
 }
 
 export default Message
+
+export const StyledHeadline = styled.h2`
+    ${({ theme }) => theme.breakpoint.media.base`
+        color: ${theme.colors.black};
+        text-align: center;
+        font-size: ${theme.fonts.size.xl};
+    `}
+    ${({ theme }) => theme.breakpoint.media.ss`
+        color: ${theme.colors.yellow};
+        text-align: center;
+        font-size: ${theme.fonts.size.xl};
+    `}
+    ${({theme}) => theme.breakpoint.media.sm`
+        color: ${theme.colors.blue};
+        text-align: center;
+        font-size: ${theme.fonts.size.xl2};
+    `}
+    ${({theme}) => theme.breakpoint.media.md`
+        color: ${theme.colors.primary};
+        text-align: center;
+        font-size: ${theme.fonts.size.xl3};
+    `}
+    ${({theme}) => theme.breakpoint.media.lg`
+        color: ${theme.colors.red};
+        text-align: center;
+        font-size: ${theme.fonts.size.xl4};
+    `}
+    ${({theme}) => theme.breakpoint.media.xl`
+        color: ${theme.colors.green};
+        text-align: center;
+        font-size: ${theme.fonts.size.xl5};
+    `}
+`
+
+export const StyledMessage = styled.h3`
+    ${({ theme }) => theme.breakpoint.media.base`
+        color: ${theme.colors.black};
+        text-align: left;
+        font-size: ${theme.fonts.size.base};
+        overflow-wrap: break-word;
+        padding-bottom: 1%;
+        padding-left: 8%;
+        padding-right: 8%;
+    `}
+`
