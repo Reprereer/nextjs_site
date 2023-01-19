42f779017ad0e6efeb47f974aaac7bfa0aaaf79e
reprereer <kokudoo0912@gmail.com>
2023-01-20 03:06:46 +0900

    BlogページのリスティングにStyled-componentsを追加した

--- components/postlist.jsx
+++ components/postlist.jsx
@@ -2,23 +2,37 @@ import React from 'react'
 import Date from '../components/date'
 import Link from 'next/link'
 import Youtube from 'react-youtube'
+import styled from 'styled-components'
 
 //投稿された記事のタイトル、日時をリスティングする
 export default function PostList({ allPostsData }) {
     return (
         <>
-            <ul>
+            <StyledPostlist>
                 {allPostsData.map(({ id, date, title }) => (
-                    <li key={id}>
+                    <StyledPosts key={id}>
                         <Link href={`/posts/${id}`}>{title}</Link>
                         <br/>
                         <small>
                             <Date dateString={date} />
                         </small>
-                    </li>
+                    </StyledPosts>
                 ))}
-            </ul>
+            </StyledPostlist>
         </>
     )
 }
 
+export const StyledPostlist = styled.div`
+    ${({ theme }) => theme.breakpoint.media.base`
+        text-align: center;
+        background: #ffffff;
+        margin-right: 20px;
+    `}
+`
+
+export const StyledPosts = styled.div`
+    ${({ theme }) => theme.breakpoint.media.base`
+        padding-bottom: 1%;
+    `}
+`
--- pages/blog.jsx
+++ pages/blog.jsx
@@ -3,6 +3,7 @@ import Layout from '../components/layout'
 import Head from 'next/head'
 import { getSortedPostsData } from '../lib/posts'
 import PostList from '../components/postlist'
+import styled from 'styled-components'
 
 function Blog({allPostsData}) {
     return (
@@ -15,7 +16,7 @@ function Blog({allPostsData}) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             
-            <h1>Blog</h1>
+            <StyledTitle>Blog</StyledTitle>
             <PostList allPostsData={allPostsData} />
         </Layout>
         </>
@@ -32,3 +33,26 @@ export async function getStaticProps() {
         }
     }
 }
+
+const StyledTitle = styled.h1`
+    ${({ theme }) => theme.breakpoint.media.base`
+        color: ${theme.colors.black};
+        text-align: center;
+        font-size: ${theme.fonts.size.xl2};
+    `}
+    ${({ theme }) => theme.breakpoint.media.ss`
+        font-size: ${theme.fonts.size.xl3}
+    `}
+    ${({theme}) => theme.breakpoint.media.sm`
+        font-size: ${theme.fonts.size.xl3};
+    `}
+    ${({theme}) => theme.breakpoint.media.md`
+        font-size: ${theme.fonts.size.xl4};
+    `}
+    ${({theme}) => theme.breakpoint.media.lg`
+        font-size: ${theme.fonts.size.xl4};
+    `}
+    ${({theme}) => theme.breakpoint.media.xl`
+        font-size: ${theme.fonts.size.xl4};
+    `}
+`
