361864ba17f592575ce3fe683c18c28356fb015a
reprereer <kokudoo0912@gmail.com>
2023-02-07 00:28:48 +0900

    デザインと内容をアップデートした

--- components/message.jsx
+++ components/message.jsx
@@ -5,7 +5,7 @@ const Message = () => {
     return (
         <section>
             <StyledMessage>
-            ここに載っている創作物は私の制作したフィクションです。ブログ記事も、小説も、音楽も、動画も、そしてこのサイトすらフィクションです。あなたが見ている物語のキャラクターは私とは全く関係がありません。
+            The characters and events depicted in this site are fictitious. Any similarity to actual persons, living or dead, is purely coincidental.
             </StyledMessage>
         </section>
     )
--- components/projects.jsx
+++ components/projects.jsx
@@ -12,11 +12,11 @@ const Projects = () => {
             <StyledContentBox>
                 <StyledColumn>
                     <StyledHeadlineSmall>Music</StyledHeadlineSmall>
-                    <StyledText>Tech Treurica（てくとれうりか）の名義で音楽活動をしています。ボーカルからインストまでジャンル問わず幅広く創作していきます。作った曲はYouTube、Soundcloudにて視聴可能となっております。</StyledText>
+                    <StyledText>Tech Treurica（てくとれうりか）の名義で音楽活動をしています。ボーカルからインストまでジャンル問わず幅広く創作していきます。作った曲はYouTube、Soundcloudにて視聴可能となっております。サイト右上のメニューバーより”Music”を選択するかサイト下部よりアイコンをクリックしてご確認ください。</StyledText>
                 </StyledColumn>
                 <StyledColumn>
                     <StyledHeadlineSmall>Blog</StyledHeadlineSmall>
-                    <StyledText>本サイト「この物語はフィクションです」の中でブログを運営しています。投稿頻度は週1回程度の予定ですが、作っているもの次第で変わりますのでご了承ください。小説やアイデア記事など様々なジャンルの投稿をお楽しみください。</StyledText>
+                    <StyledText>本サイト「この物語はフィクションです」の中でブログを運営しています。投稿頻度は週1回程度の予定ですが、作っているもの次第で変わりますのでご了承ください。小説やアイデア記事など様々なジャンルの投稿をお楽しみください。サイト右上のメニューバーより”Blog”を選択し記事一覧から様々な記事を読むことができます。ご確認ください。</StyledText>
                 </StyledColumn>
             </StyledContentBox>
         </section>
